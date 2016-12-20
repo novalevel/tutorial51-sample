3059d623e297a1d0f48b43342d28b0b6baad3304
feliks@homestead <547276540@qq.com>
2016-12-20 00:59:27 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -10,6 +10,12 @@ use Auth;
 
 class SessionsController extends Controller
 {
+      public function __construct()
+      {
+        $this->middleware('guest',[
+          'only' => ['create']
+        ]);
+      }
       public function create()
       {
           return view('sessions.create');
@@ -30,7 +36,9 @@ class SessionsController extends Controller
       if (Auth::attempt($credentials,$request->has('remember'))) {
           session()->flash('success', '欢迎回来！');
           // TODO 这里不明白，为什么可以这样伟参，route 定义是是 id 呀！
-          return redirect()->route('users.show', [Auth::user()]);
+          // return redirect()->route('users.show', [Auth::user()]);
+          // TODO 更友好的转向：返回到上一次尝试访问的页面
+          return redirect()->intended(route('users.show', [Auth::user()]));
       } else {
           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
           return redirect()->back();
